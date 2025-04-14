13f7550e0016d9ec1a86f78f2d75fdf3c7ba217e
MOHD ARHAM <119915873+ARHAM7060@users.noreply.github.com>
2025-04-14 22:56:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import os
+import os #To provide fuctions to interact with operating system
 import pickle
 import streamlit as st
-import requests
-import tempfile
+import requests #To send HTTP requests and interact with web APIs easily
+import tempfile #To create temporary files and directories that are automatically cleaned up, useful for storing data during runtime
 
 # For downloading from Google Drive
 try:
@@ -13,17 +13,17 @@ except ImportError:
 
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=282f9c8c31adb71767c4c6ff39e14a23&language=en-US"
-    data = requests.get(url).json()
-    poster_path = data.get('poster_path')
-    return "https://image.tmdb.org/t/p/w500/" + poster_path if poster_path else ""
+    data = requests.get(url).json() #Data from url to json format
+    poster_path = data.get('poster_path') #json from url have poster_path key which has poster link as its value is what we get
+    return "https://image.tmdb.org/t/p/w500/" + poster_path if poster_path else "" #To concatenate base link and posterpath
 
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    index = movies[movies['title'] == movie].index[0] #Index of selected movie
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1]) #distances with indexes and sort by similarity value by using key
     recommended_movie_names = []
     recommended_movie_posters = []
     for i in distances[1:6]:
-        movie_id = movies.iloc[i[0]].movie_id
+        movie_id = movies.iloc[i[0]].movie_id #access the value of movie_id column value 
         recommended_movie_names.append(movies.iloc[i[0]].title)
         recommended_movie_posters.append(fetch_poster(movie_id))
     return recommended_movie_names, recommended_movie_posters
